e988d5dfea8010e2b2c14784fdec1d6ca2d93d9c
tahmed-vassar <55174032+tahmed-vassar@users.noreply.github.com>
2021-12-19 18:26:58 +0100

    Delete AndWeather directory

--- AndWeather/app/src/main/java/com/example/andweather/data/Result.kt
+++
@@ -1,4 +0,0 @@
-package com.example.andweather.data
-
-data class Result(var name: String, var country: String)
-
--- AndWeather/app/src/main/java/com/example/andweather/touch/TouchHelperCallback.kt
+++
@@ -1,6 +0,0 @@
-package com.example.andweather.touch
-
-interface TouchHelperCallback {
-    fun onDismissed(position: Int)
-    fun onItemMoved(fromPosition: Int, toPosition: Int)
-}
